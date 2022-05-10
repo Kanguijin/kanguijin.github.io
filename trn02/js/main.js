53f86f03b1d98ef2cef928bd604ab276dccd2005
Kanguijin <98031055+Kanguijin@users.noreply.github.com>
2022-05-10 14:41:47 +0900

    Add files via upload

---
+++ trn02/js/main.js
@@ -0,0 +1,51 @@
+$(function () {
+
+    /////////////////////////////////////
+
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp(2000);
+
+    })
+
+
+    $('.main_slider').on('init reInit afterChange', function () {
+        let current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on')
+    })
+
+    $('.main_slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    $('#myMovie').YTPlayer({
+        videoURL: 'https://youtu.be/Qwsh9eZDLyc',
+        containment: '.youtube',
+        autoPlay: true,
+        mute: true, 
+        startAt: 0, 
+        opacity: 1, 
+        showControls: false,
+        playOnlyIfVisible: true
+    });
+
+    $('.movie .dec i:nth-of-type(1)').on('click', function () {
+        $('.movie video').trigger('play');
+    })
+    $('.movie .dec i:nth-of-type(2)').on('click', function () {
+        $('.movie video').trigger('pause');
+    })
+
+
+    $('.youtube i:nth-of-type(1)').on('click', function () {
+        $('#myMovie').YTPPlay();
+    })
+    $('.youtube i:nth-of-type(2)').on('click', function () {
+        $('#myMovie').YTPPause();
+    })
+
+    /////////////////////////////////////
+})
